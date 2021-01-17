74113818a84b977f49a152c11e8dc52a89ed0dd1
brian891005 <kenmill1005@gmail.com>
2021-01-17 10:39:11 +0800

    add crawler.js

--- 爬蟲/01.js
+++ 爬蟲/01.js
@@ -1,3 +1,5 @@
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
+
 async function getPage(url) {
     const res = await fetch(url);
     return await res.text();
@@ -20,7 +22,7 @@ async function getPage(url) {
     var m=0 //第幾個子網頁的網址
     var root1=[]
     var j=0
-    for (let i=0; i<urlList.length; i++) {
+    for (let i=0; i<20; i++) {
       var url = urlList[i]
       var x=0 //flag是否抓到重複網址
       console.log('第'+(m+1)+'子網頁')
@@ -72,6 +74,20 @@ async function getPage(url) {
   var urlList = [
     'https://www.itread01.com/content/1549194305.html', 
   ]
-  
-  await craw(urlList)
+await craw(urlList)
+const db = new DB("test.db");
+db.query("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
+
+
+// Run a simple query
+for (var name of urlList)
+  db.query("INSERT INTO people (name) VALUES (?)", [name]);
+
+// Print out data in table
+for (const [id, name] of db.query("SELECT id, name FROM people"))
+  console.log(id, name);
+
+// Close connection
+db.close();
+
   
